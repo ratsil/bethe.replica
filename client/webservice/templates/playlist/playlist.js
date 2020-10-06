ed3aa30acf7a8a2906a70175c74ccab8fbcc1957
Alex <al.hudyakov@gmail.com>
2020-10-06 08:20:06 +0300

    kpp iteration interface

--- client/webservice/templates/playlist/playlist.js
+++ client/webservice/templates/playlist/playlist.js
@@ -17,11 +17,46 @@ $(document).ready(function () {
         scrollY: 400
     });
 
-    
+//если у нас есть кука плейлиста, то открываем вкладку которая прописана в куке
+//иначе и по дефолту открываем вкладку onAir
+aStatuses = [{nID: 1, sName: "Запланировано"},
+{nID: 2, sName: "Поставлено в очередь"},
+{nID: 3, sName: "Подготовлено"},
+{nID: 4, sName: "В эфире"},
+{nID: 5, sName: "Проиграно"},
+{nID: 6, sName: "Пропущено"},
+{nID: 7, sName: "С ошибкой"}]
 
-    dtToday = new Date();
-    PL.Items.ArchiveList( dtStart, dtToday, o => {
-        
-    })
+    if (Cookies.Get('plFoder')) {
+        let plFolder = Cookies.Get('plFoder');
+        dtStart = new Date();
+        switch (plFolder) {
+            case 'Archive':
+                dtToday = new Date();
+                PL.Items.ArchiveList(dtStart, dtToday, o => {
+                    alert('воу! щас будем смотреть архив!');
+                })
+                break;
+            case 'Plan':
+                dtToday = new Date();
+                PL.Items.PlannedList(dtStart, dtToday, o => {
+                    alert('воу! щас будем смотреть план!');
+                })
+                break;
+            default:
+                dtToday = new Date();
+                PL.Items.List(aStatuses, o => {
+                    alert('воу! щас будем смотреть то, что в эфире!');
+                })
+        }
+    } else {
+        dtToday = new Date();
+        // API.MAM.Statuses.List (aStatuses => {
+            API.PL.Items.List(aStatuses, o => {
+                alert('воу! щас будем смотреть то, что в эфире!');
+            })  
+        // })
+
+    }
 
 });
