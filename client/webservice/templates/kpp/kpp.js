c022b087e3dc1895cf706213be3daefc8e8137f4
Alex <al.hudyakov@gmail.com>
2020-10-06 08:03:29 +0300

    kpp page start

---
+++ client/webservice/templates/kpp/kpp.js
@@ -0,0 +1,46 @@
+//# sourceURL=kpp.js
+
+$(document).ready(function() {
+
+    // load template kpp-files
+    Templates.Attach('kpp/kpp-files.html', $('[name=filesSection]'), function() {});
+
+    $("#collapseTwo").on('shown.bs.collapse', function() {
+        // load template kpp-addition
+        $('[name=additionSection]').empty();
+        Templates.Attach('kpp/kpp-addition.html', $('[name=additionSection]'), function() {});
+    })
+
+    $("#collapseThree").on('shown.bs.collapse', function() {
+        // load template kpp-tasks
+        $('[name=tasksSection]').empty();
+        Templates.Attach('kpp/kpp-tasks.html', $('[name=tasksSection]'), function() {});
+    })
+
+
+    if (Cookies.Get('kppFoder')) {
+        let kppFolder = Cookies.Get('kppFoder');
+        switch (kppFolder) {
+            case 'Files':
+                dtToday = new Date();
+
+                break;
+            case 'Addition':
+                dtToday = new Date();
+
+                break;
+            case 'Tasks':
+                dtToday = new Date();
+
+                break;
+            default:
+                dtToday = new Date();
+
+        }
+    } else {
+        dtToday = new Date();
+
+
+    }
+
+})
