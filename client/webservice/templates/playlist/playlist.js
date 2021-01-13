5c1cbec9c2d07e9f8b340a1eddec5254a43a8bbe
Alex <al.hudyakov@gmail.com>
2021-01-13 23:57:00 +0300

    add menu in playlist

--- client/webservice/templates/playlist/playlist.js
+++ client/webservice/templates/playlist/playlist.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
 
+
     // кнопка Import
     $('[name=playlistImport]').click(function() {
         $('#playListImport').modal('show');
@@ -69,7 +70,136 @@ $(document).ready(function() {
         loadOnAirInTable();
     }
 
+    // при клике на строку в таблице 
+    // запоминаем объект строки в куки или берем ID из первого td
+    // вызываем окно с меню
+    $(document).on('click', '#onPlannedTable tr', function(e) {
+        let parent = e.target.parentNode;
+        let nId = parseInt(parent.firstElementChild.innerHTML);
+        positionMenu(e);
+        toggleMenuOn();
+    });
+
+    /**
+     * Listens for keyup events.
+     */
+
+    /**
+     * Va riables.
+     */
+    let contextMenuClassName = "context-menu";
+    let contextMenuItemClassName = "context-menu__item";
+    let contextMenuLinkClassName = "context-menu__link";
+    let contextMenuActive = "context-menu--active";
+    let menu = document.querySelector("#context-menu");
+    let menuItems = menu.querySelectorAll(".context-menu__item");
+    let menuState = 0;
+    let menuWidth;
+    let menuHeight;
+    let menuPosition;
+    let menuPositionX;
+    let menuPositionY;
+
+    let windowWidth;
+    let windowHeight;
+
+    function keyupListener() {
+        window.onkeyup = function(e) {
+            if (e.keyCode === 27) {
+                toggleMenuOff();
+            }
+        }
+    }
+
+    /**
+     * Window resize event listener
+     */
+    function resizeListener() {
+        window.onresize = function(e) {
+            toggleMenuOff();
+        };
+    }
+
+    /**
+     * Turns the custom context menu on.
+     */
+    function toggleMenuOn() {
+        if (menuState !== 1) {
+            menuState = 1;
+            menu.classList.add(contextMenuActive);
+        }
+    }
+
+    /**
+     * Turns the custom context menu off.
+     */
+    function toggleMenuOff() {
+        if (menuState !== 0) {
+            menuState = 0;
+            menu.classList.remove(contextMenuActive);
+        }
+    }
+
+    /**
+     * Get's exact position of event.
+     * 
+     * @param {Object} e The event passed in
+     * @return {Object} Returns the x and y position
+     */
+    function getPosition(e) {
+        let posx = 0;
+        let posy = 0;
+
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        } else if (e.clientX || e.clientY) {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+
+        return {
+            x: posx,
+            y: posy
+        }
+    }
+
+    /**
+     * Positions the menu properly.
+     * 
+     * @param {Object} e The event
+     */
+    function positionMenu(e) {
+        clickCoords = getPosition(e);
+        clickCoordsX = clickCoords.x;
+        clickCoordsY = clickCoords.y;
+
+        toggleMenuOn();
+
+        menuWidth = menu.offsetWidth + 4;
+        menuHeight = menu.offsetHeight + 4;
+
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+
+        if ((windowWidth - clickCoordsX) < menuWidth) {
+            menu.style.left = windowWidth - menuWidth + "px";
+        } else {
+            menu.style.left = clickCoordsX + "px";
+        }
+
+        if ((windowHeight - clickCoordsY) < menuHeight) {
+            menu.style.top = windowHeight - menuHeight + "px";
+        } else {
+            menu.style.top = clickCoordsY + "px";
+        }
+    }
+
+    keyupListener();
+
 });
+
+
 
 // таблица закладки OnAir
 function loadOnAirInTable() {
